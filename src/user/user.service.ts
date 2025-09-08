b16ec338d82a773749b6e225ec17ca93dd0844c5
Daniel dos Santos C <danielsantos@sysfar.com.br>
2025-09-07 22:29:56 -0300

    feat: implementado hash de senha no service de user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { User } from './interfaces/user.interface';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UserService {
     private users: User[] = [];
 
     async createUser(createUserDto: CreateUserDto): Promise<User> {
-        return ({
+        const saltRounds = 10;
+        const passwordHash = await hash(createUserDto.password, saltRounds);
+
+        const user: User = {
             ...createUserDto,
-            id: 1
-        })
+            password: passwordHash,
+            id: this.users.length + 1
+        }
+
+        this.users.push(user)
+
+        return user
+    }
+
+    async getAllUsers(): Promise<User[]> {
+        return this.users;
     }
 }
