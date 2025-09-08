86a669ed28295a7b31eecc07918aaecbe8355c68
Daniel dos Santos C <danielsantos@sysfar.com.br>
2025-09-07 22:43:02 -0300

    feat: instalado typeorm e config acesso a banco pg (docker)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-    imports: [UserModule],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DB_HOST || 'localhost',
+            port: parseInt(process.env.DB_PORT || '5433', 10),
+            username: process.env.DB_USERNAME || 'postgres',
+            password: process.env.DB_PASSWORD || 'postgres',
+            database: process.env.DB_DATABASE || 'postgres',
+            autoLoadEntities: true,
+            synchronize: true,
+        }),
+        UserModule,
+    ],
     controllers: [],
     providers: [],
 })
